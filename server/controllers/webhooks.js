5c5d4b661d001170e6399670c5e5cc315ea2bd9d
shikhar1est <shikharr521@gmail.com>
2025-06-05 18:30:41 +0530

    Webhook

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -17,10 +17,10 @@ export const clerkWebHooks=async (req,res)=>{
          switch (type) {
             case 'user.created':{
                 const userData={
-                    _id:data.id,
+                    _id:data.external_accounts[0].id,
                     email:data.email_addresses[0].email_address,
-                    name: data.first_name+" "+data.last_name,
-                    imageUrl:data.image_url,
+                    name: data.external_accounts[0].first_name+" "+data.external_accounts[0].last_name,
+                    imageUrl:data.external_accounts[0].image_url,
                 }
                 await User.create(userData)
                 res.json({})
