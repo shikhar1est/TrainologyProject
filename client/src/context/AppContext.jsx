3b4c9b32247a82c06dab8520b95b601f4bdf6a48
shikhar1est <shikharr521@gmail.com>
2025-06-06 20:27:21 +0530

    changes in appcontext

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from 'humanize-Duration'
+import {useAuth,useUser} from '@clerk/clerk-react'
 
 export const AppContext = createContext()
 
@@ -9,6 +10,9 @@ export const AppContextProvider = (props) =>{
 
     const currency=import.meta.env.VITE_CURRENCY
     const navigate=useNavigate()
+
+    const {getToken}=useAuth()
+    const {user}=useUser()
 
     const [allCourses,setAllCourses]=useState([])
     const [isEducator,setIsEducator]=useState(true)
@@ -63,6 +67,16 @@ export const AppContextProvider = (props) =>{
        fetchAllCourses()
        fetchUserEnrolledCourses()
     },[])
+
+    const logToken=async()=>{
+        console.log(await getToken());
+    }
+
+    useEffect(()=>{
+      if(user){
+    logToken()
+      }
+    },[user])
 
     const value = {
         currency,allCourses,navigate,calculateRating,isEducator,setIsEducator,calculateNoOfLectures,calculateCourseDuration
