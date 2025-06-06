7225897c9fefb1d2b0cd76d5339fc16b7a6f9869
shikhar1est <shikharr521@gmail.com>
2025-06-06 20:45:40 +0530

    updated routes

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,7 +1,7 @@
 import {clerkClient} from '@clerk/express'
 
 //Update Role to Educator
-export const updateRoleToEducator=async()=>{
+export const updateRoleToEducator=async(req,res)=>{
     try {
         const userId=req.auth.userId
         await clerkClient.users.updateUserMetadata(userId,{
@@ -14,3 +14,4 @@ export const updateRoleToEducator=async()=>{
         res.json({success : false,message : error.message})
     }
 }
+
